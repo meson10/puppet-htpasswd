6e5030e6d6924dec77e90e6fab5b995df79e5870
andrefsp <andrefsp@gmail.com>
2012-09-24 16:47:03 +0100

    added encryption and user deletion support on the module

--- README.rst
+++ README.rst
@@ -5,13 +5,35 @@ puppet-htpasswd
 A simple puppet module to handle htpasswd command line tool
 
 
-example
-=======
+examples
+========
 
+Create or updating an entry on the auth file
 ::
-
+    
     htpasswd::user {"username":
         file => '/path_to_my_file',
         password => 'username_password'
     }
+    
+
+Deleting an user
+::
+    
+     htpasswd::user {"username":
+        file => '/path_to_my_file',
+        ensure => absent
+    }
+    
+
+Controling the encryptation, options are(`md5`, `sha`, `plain`, `crypt`)
+::
+    
+      htpasswd::user {"username":
+        file => '/path_to_my_file',
+        ensure => absent,
+        encryptation => 'sha'
+    }
+      
+
 
--- htpasswd/manifests/user.pp
+++ htpasswd/manifests/user.pp
@@ -1,4 +1,4 @@
-define httpasswd::user($password, $file)
+define htpasswd::user($password, $file, $ensure = present, $encryption = 'md5')
 {
     Exec{
         path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
@@ -9,9 +9,23 @@ define httpasswd::user($password, $file)
     file{$file:
         ensure => present,
     }
+    
+    case $encryption {
+        'md5':      {   $enctype = "-m" }
+        'sha':      {   $enctype = "-s" }
+        'crypt':    {   $enctype = "-d" }
+        'plain':    {   $enctype = "-p" }
+        default:    {   $enctype = "-m" } 
+    }    
 
-    exec {"create_user":
-        command => "htpasswd -b $file ${name} ${password}",
+    case $ensure {
+        absent:     {   $cmd = "htpasswd -b -D $file ${name}" }
+        present:    {   $cmd = "htpasswd -b ${enctype} $file ${name} ${password}" }
+        default:    {   $cmd = "htpasswd -b ${enctype} $file ${name} ${password}" }
+    }
+
+    exec {"manage_user":
+        command => "${cmd}",
         require => [Package['apache2-utils'], File[$file]],
     }
 }
